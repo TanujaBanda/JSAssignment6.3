c5b02a3e16e2251c0bde2427a7e04118a02bbaac
TanujaBanda <tanujakumari24m@gmail.com>
2017-01-09 13:38:25 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var sum=0;
+for(var i=0;i<=1000;i++)
+	sum+=i;
+var j=1,sum1=0;
+while(j>0 && j<=1000)
+	sum1+=j;
+console.log("The sum upto 1000 using for loop is"+sum);
+console.log("The sum upto 1000 using while loop is"+sum1);
